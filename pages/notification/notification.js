43c58b15d277fc6babecd4423860caa93ac78e40
Genening <zhangjlin5@mail2.sysu.edu.cn>
2019-03-23 23:39:10 +0800

    homepage

--- pages/notification/notification.js
+++ pages/notification/notification.js
@@ -5,7 +5,14 @@ Page({
    * Page initial data
    */
   data: {
-
+    value1: '',
+    value2: '',
+    value3: '',
+    value4: '',
+    value5: '',
+    value6: '',
+    value7: '',
+    value8:''
   },
 
   /**
